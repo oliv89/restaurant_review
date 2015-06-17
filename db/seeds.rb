d47fe044baf1489dcf72e5a6540ad4b530761eec
Olivia Brami <olivia.brami@gmail.com>
2015-06-17 18:47:34 +0200

    restaurant rails + navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+10.times do
+  restaurant = Restaurant.new({
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  })
+  restaurant.save!
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
